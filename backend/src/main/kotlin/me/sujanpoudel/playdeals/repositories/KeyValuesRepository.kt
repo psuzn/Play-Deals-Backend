21c79beef1ab58d565ec08a7d1b5bf92d0363421
Sujan Poudel <mail@sujanpoudel.me>
2023-10-03 11:56:54 +0545

    remove support for any serializable object on key value store (#28)

--- backend/src/main/kotlin/me/sujanpoudel/playdeals/repositories/KeyValuesRepository.kt
+++ backend/src/main/kotlin/me/sujanpoudel/playdeals/repositories/KeyValuesRepository.kt
@@ -1,18 +1,9 @@
 package me.sujanpoudel.playdeals.repositories
 
-import me.sujanpoudel.playdeals.domain.entities.KeyValueEntity
-import kotlin.reflect.KClass
-
 interface KeyValuesRepository {
-  suspend fun <T : Any> set(key: String, value: T, clazz: KClass<out T> = value::class): KeyValueEntity<T>
+  suspend fun set(key: String, value: String): String
 
-  suspend fun <T : Any> get(key: String, clazz: KClass<T>): T?
+  suspend fun get(key: String): String?
 
   suspend fun delete(key: String)
 }
-
-suspend inline fun <reified T : Any> KeyValuesRepository.set(key: String, value: T): KeyValueEntity<T> {
-  return set(key, value, T::class)
-}
-
-suspend inline fun <reified T : Any> KeyValuesRepository.get(key: String) = get(key, T::class)
