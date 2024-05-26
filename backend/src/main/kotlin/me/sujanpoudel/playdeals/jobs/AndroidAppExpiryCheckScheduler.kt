94dd013049028fb055685a9838cc424cb5755b05
Sujan Poudel <mail@sujanpoudel.me>
2024-05-26 09:05:26 +0545

    add job deletion for succeded and failed jobs (#53)

--- backend/src/main/kotlin/me/sujanpoudel/playdeals/jobs/AndroidAppExpiryCheckScheduler.kt
+++ backend/src/main/kotlin/me/sujanpoudel/playdeals/jobs/AndroidAppExpiryCheckScheduler.kt
@@ -2,16 +2,22 @@ package me.sujanpoudel.playdeals.jobs
 
 import me.sujanpoudel.playdeals.common.SIMPLE_NAME
 import me.sujanpoudel.playdeals.common.loggingExecutionTime
+import me.sujanpoudel.playdeals.logger
 import me.sujanpoudel.playdeals.repositories.DealRepository
 import org.jobrunr.jobs.lambdas.JobRequest
+import org.jobrunr.jobs.states.StateName
 import org.jobrunr.scheduling.JobRequestScheduler
 import org.jobrunr.scheduling.RecurringJobBuilder
+import org.jobrunr.storage.StorageProvider
 import java.time.Duration
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 class AndroidAppExpiryCheckScheduler(
   private val repository: DealRepository,
-  private val requestScheduler: JobRequestScheduler
+  private val requestScheduler: JobRequestScheduler,
+  private val storageProvider: StorageProvider
 ) : CoJobRequestHandler<AndroidAppExpiryCheckScheduler.Request>() {
 
   override suspend fun handleRequest(jobRequest: Request): Unit = loggingExecutionTime(
@@ -21,6 +27,10 @@ class AndroidAppExpiryCheckScheduler(
       .map { AppDetailScrapper.Request(it.id) }
 
     requestScheduler.enqueue(apps)
+
+    val lastUpdatedTime = Instant.now().minus(1, ChronoUnit.HOURS)
+    val jobs = storageProvider.deleteJobsPermanently(StateName.FAILED, lastUpdatedTime)
+    logger.info("deleted FAILED `$jobs`")
   }
 
   class Request private constructor() : JobRequest {
